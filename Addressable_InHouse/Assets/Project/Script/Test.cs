fb4c3c5783313438827c449b5deadc684e2842e9
ChigameStudio <chigame.studio@gmail.com>
2021-01-26 22:34:39 +0900

    Addressableのロードテンプレート作成

---
+++ Addressable_InHouse/Assets/Project/Script/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private AddressableObject<GameObject> obj_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj_ = AddressableCore.CreateAddressable<GameObject>("Assets/Project/AppData/Cube.prefab");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj_.LoadStart();
+        obj_.Instance();
+        if (obj_.IsSetUp == true)
+        {
+            Debug.Log(obj_.GetObject.gameObject);
+        }
+        obj_.Release();
+    }
+}
